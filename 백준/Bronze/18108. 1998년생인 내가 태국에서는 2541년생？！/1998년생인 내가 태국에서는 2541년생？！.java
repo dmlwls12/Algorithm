d121ccc1d3335f7c01e5fcc78e641afdf73d9f4f
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2024-09-29 21:32:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner year = new Scanner(System.in);
+        int buddhistEra = year.nextInt();      
+
+        System.out.println(buddhistEra - 543);
+
+
+    }
+}
