b9f67cdbf41a28f8f7a112af7692f2480dbe6276
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2024-10-03 18:41:26 +0900

    [Bronze V] Title: A+B - 5, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        for (;;) {
+
+            String[] input = br.readLine().split(" ");
+
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+            
+            System.out.println(a + b);
+            
+        }
+        System.out.println();
+    }
+}
