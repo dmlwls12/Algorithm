9b2dd556a8d6224b516285992e71ed2262336da6
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2024-10-07 18:01:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 472 ms, Memory: 92392 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,39 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int num : arr) {
+
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        bw.write(min + " " + max);
+        bw.newLine();
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
