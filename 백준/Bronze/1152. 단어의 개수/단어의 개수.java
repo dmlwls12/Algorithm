18ca77ee599651398f5dcc390c7c6848d622d345
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2024-10-14 17:28:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 20176 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+
+        bw.write(String.valueOf(st.countTokens()));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+}
