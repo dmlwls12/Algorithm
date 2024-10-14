d26b44589bbdb36a22fb96afc4f26fd3f3388ab3
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2024-10-14 18:10:21 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -10,36 +10,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s1 = st.nextToken();
+        String s2 = st.nextToken();
 
-        int n = st.countTokens();
-        int[] intArr = new int[n];
+        int a = Integer.parseInt(new StringBuilder(s1).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(s2).reverse().toString());
 
-        for (int i = 0; i < n; i++) {
-            intArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] reversedArr = new int[intArr.length];
-
-        // 자리 바꾸기
-        for (int i = 0; i < intArr.length; i++) {
-            int temp = intArr[i];
-
-            int ones = temp % 10;
-            int tens = (temp / 10) % 10;
-            int hundreds = (temp / 100) % 10;
-
-            reversedArr[i] = ones * 100 + tens * 10 + hundreds;
-        }
-
-        int max = reversedArr[0];
-
-        for(int num : reversedArr) {
-            if(num > max) max = num;
-        }
-
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(a > b ? a : b));
 
         bw.flush();
         br.close();
