c6c480bf5040f5d5f3f504da4713af4bdec3c44e
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2025-02-27 15:49:05 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 27.39 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < number.length(); i++){
+            int n = Character.getNumericValue(number.charAt(i));
+            
+            sum += n;
+            
+            answer = sum % 9;
+            
+        }
+        return answer;
+    }
+}
