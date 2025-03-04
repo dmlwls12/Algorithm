38aa74b133c62e17e0b867b35eb271f9e564c7b7
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2025-03-04 13:57:48 +0900

    [level 0] Title: 접미사 배열, Time: 0.64 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        ArrayList<String> answer = new ArrayList<>();
+        String[] answer = new String[my_string.length()];
         
         for(int i = 0; i < my_string.length(); i++){
-            String str = my_string.substring(i);
-            answer.add(str);
-            Collections.sort(answer);
+            answer[i] = my_string.substring(i);
         }
-        return answer.stream().toArray(String[]::new);
+        Arrays.sort(answer);
+        
+        return answer;
     }
 }
