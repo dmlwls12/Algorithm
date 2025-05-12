0b402dd8710b2a312086216850222f3cf8407955
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2025-05-12 13:22:43 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -5,13 +5,8 @@ class Solution {
         for(int i = 0; i < answer.length; i++){
             for(int j = i; j < answer[0].length; j+=n){
                 answer[i][j] = 1;
-                
             }
-            
-            
         }
-        
-        
         return answer;
     }
 }
