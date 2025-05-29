0746fdd5aea6ec84383b2a53365df3a1940bf313
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2025-05-29 09:24:28 +0900

    [level 1] Title: 폰켓몬, Time: 4.84 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,17 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
         
+        // 1. nums 중 n/2마리만 가져갈 수 있다.
+        // 2. 경우의 수 반환
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        
         int answer = nums.length / 2;
-        
-        HashSet<Integer> numSet = new HashSet<>();
-        
-        for(int num : nums) {
-            numSet.add(num);
-        }
-        
-        if(numSet.size() > answer) {
-            return answer;
-        } else {
-            return numSet.size();
-        }
-        
+        return Math.min(set.size(), answer);
     }
 }
