68947dcec060dbccfb5fa3175bcdd790b38de4ee
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2025-05-29 10:38:01 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,24 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         
+        // 1. Map 이용
+        // 2. 종류별로 하나씩만 매칭
+        // 경우의 수 반환
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String[] s : clothes){
+            String type = s[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
         int answer = 1;
-        
-        HashMap<String, Integer>  closet = new HashMap<>();
-        
-        for(String[] setUp : clothes){
-            String type = setUp[1];
-            closet.put(type, closet.getOrDefault(type, 0) + 1);
+        for(int count : map.values()){
+            answer *= count + 1;
         }
         
-        for(int count : closet.values()){
-            answer *= (count + 1);
-        }
         return answer - 1;
     }
 }
