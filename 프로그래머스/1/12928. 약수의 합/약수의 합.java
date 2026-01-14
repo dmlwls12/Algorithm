0fc15f781f69adb8d075d7e4afdcfc24d70d3b10
dmlwls12 <162261678+dmlwls12@users.noreply.github.com>
2026-01-14 16:12:02 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,10 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0) set.add(i);
+        }
+        
         int answer = 0;
         
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0) answer += i;
-        }
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()) answer += iterator.next();
         return answer;
     }
 }
